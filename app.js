990ff04552cd82d157b086761c4b84ce65d040e8
Marcos-MEDENOU <marcosmedenou@gmail.com>
2024-02-22 20:56:24 +0100

    @maj

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const path = require("path")
 const app = express();
+app.use(express.static('css'));
 
 //Montrer a Express.js ou se trouvent les vues
+app.use("/img", express.static(path.join(__dirname, "img")));
 app.set('views', path.join(__dirname, 'views'))
 //Demander a express d'utiliser pug comme engin de template
 app.set('view engine', 'pug')
@@ -10,6 +12,10 @@ app.set('view engine', 'pug')
 app.get('/', (request,response)=>{
     // response.send("<h1>hello</h1>")
     response.render("index")
+})
+app.get('/chat', (request,response)=>{
+    // response.send("<h1>hello</h1>")
+    response.render("chat")
 })
 
 
